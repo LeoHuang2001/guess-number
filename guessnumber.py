c14121bc0c0768ad79dc56e65bd2f08b24c99d7f
Leo Huang <huang900508@gmail.com>
2021-11-13 12:46:07 +0800

    增加數過幾次

--- guessnumber.py
+++ guessnumber.py
@@ -1,12 +1,16 @@
 import random
 x = random.randint(1,100)
-print(x)
+print(x)#正確答案
+count = 0
 while(1):
+    count += 1
     a = int(input("請輸入數字: "))
     if(a < x):
         print("比答案小")
     if(a > x):
         print("比答案大")
     if(a == x):
-        print("猜對了")
+        print("猜對了!!!!")
+        print("這是你猜的第", count, "次\n")
         break
+    print("這是你猜的第", count, "次\n")
